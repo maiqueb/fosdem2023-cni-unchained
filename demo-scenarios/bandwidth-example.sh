f49b87fb060d767648d78abc27ceb4f30794f2fa
Daniel Mellado <dmellado@redhat.com>
2023-01-19 18:15:35 +0100

    Bundle plugin binaries and cnitool
    This commit adds some dependencies and sanitizes
    the code to actually have bundled cnitool and a
    current set of cni plugins.

--- demo-scenarios/bandwidth-example.sh
+++ demo-scenarios/bandwidth-example.sh
@@ -7,8 +7,11 @@ fi
 
 bandwidth_net_name="$1"
 
-export NETCONFPATH="${NETCONFPATH:-(pwd)/examples}"
-export CNI_PATH="${CNI_PATH:-../cni-plugins/bin}"
+export NETCONFPATH="${NETCONFPATH:-$(pwd)/examples}"
+export CNI_PATH="${CNI_PATH:-$(pwd)/plugins/bin}"
+export CNI_DIR=$(pwd)/plugins
+export CNI_PATH="${CNI_PATH:-$CNI_DIR/bin}"
+export CNITOOL_BINARY=$CNI_DIR/cnitool
 
 trap "sudo ip netns del server ; sudo ip netns del client" EXIT
 clientIP="192.168.200.2"
@@ -16,21 +19,21 @@ serverIP="192.168.200.3"
 
 # start the server "pod"
 sudo ip netns add server
-sudo NETCONFPATH=$(echo $NETCONFPATH) \
-    CNI_PATH=$(echo $CNI_PATH) \
+sudo NETCONFPATH=$NETCONFPATH \
+    CNI_PATH=$CNI_PATH \
     CNI_ARGS="IgnoreUnknown=true;IP=$serverIP/24" \
     CNI_IFNAME=ens123 \
     CNI_CONTAINERID=$(uuidgen) \
-    $(which cnitool) add $bandwidth_net_name "/var/run/netns/server"
+    $CNITOOL_BINARY add $bandwidth_net_name "/var/run/netns/server"
 sudo ip netns exec server bash -c "iperf3 -s -p 9000 -1 &"
 
 # start the client "pod"
 sudo ip netns add client
 sudo NETCONFPATH=$(echo $NETCONFPATH) \
-    CNI_PATH=$(echo $CNI_PATH) \
+    CNI_PATH=$CNI_PATH \
     CNI_ARGS="IgnoreUnknown=true;IP=$clientIP/24" \
     CNI_IFNAME=ens123 \
     CNI_CONTAINERID=$(uuidgen) \
-    $(which cnitool) add $bandwidth_net_name "/var/run/netns/client"
+    $CNITOOL_BINARY add $bandwidth_net_name "/var/run/netns/client"
 sudo ip netns exec client iperf3 -c "$serverIP" -p 9000
 
